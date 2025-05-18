7e3fa06b07fd187e775cf11881087b97b8ef8566
Zane Claes <zane@claes.biz>
2025-05-18 12:15:13 -0600

    v1.2.9

--- dmx/run.sh
+++ dmx/run.sh
@@ -4,12 +4,14 @@ ls /dev/ttyUSB*
 ls -l /dev/serial/by-id/
 
 # Start OLA
-/usr/bin/olad -l 4 > /dev/null 2>&1 &
+/usr/bin/olad -l 4 &
 
 # Wait a bit for OLA to start
 sleep 3
 
 ola_dev_info
+ls /usr/lib/ola/libola*so*
+cat /home/dmxuser/.ola/ola-usbserial.conf
 
 ola_patch -d 0 -u 1
 
