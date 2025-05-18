c12853fa94c8954764fe11ffb2a6f19956e16264
Zane Claes <zane@claes.biz>
2025-05-18 11:53:19 -0600

    v1.2.2

--- dmx/run.sh
+++ dmx/run.sh
@@ -6,10 +6,12 @@ ls /dev/ttyUSB*
 ls -l /dev/serial/by-id/
 
 # Start OLA
-/usr/bin/olad --dmx-device /dev/dmx > /dev/null 2>&1 &
+/usr/bin/olad --dmx-device usb-ENTTEC_DMX_USB_PRO_EN467365-if00-port0 > /dev/null 2>&1 &
 
 # Wait a bit for OLA to start
 sleep 3
+
+ola_patch -d 0 -o 0 -u 1
 
 # Start dmx2mqtt (customize with env vars if needed)
 python3 -u /app/bridge.py
