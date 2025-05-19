00cb8e0b75012704aefcec688055a2978b437891
Zane Claes <zane@claes.biz>
2025-05-18 18:42:21 -0600

    1.3.13

--- dmx/run.sh
+++ dmx/run.sh
@@ -3,21 +3,5 @@ set -ueo pipefail
 
 sudo chgrp dmxuser /dev/ttyUSB0
 sudo chmod 666 /dev/ttyUSB0
-ls -la /dev/ttyUSB*
-ls -la /dev/serial/by-id/
 
-# Start OLA
-/usr/bin/olad -l 4 &
-
-# Wait a bit for OLA to start
-sleep 3
-
-ola_dev_info
-ls /usr/lib/ola/libola*so*
-cat /home/dmxuser/.ola/ola-usbserial.conf
-ls /usr/lib/ola/ | grep libola_
-
-ola_patch -d 0 -u 1
-
-# Start dmx2mqtt (customize with env vars if needed)
-python3 -u /app/bridge.py
+su - dmxuser -c 'HOME=/home/dmxuser start.sh'
---
+++ dmx/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -ueo pipefail
+
+echo "--- dmxuser ---"
+
+ls -la /dev/ttyUSB*
+ls -la /dev/serial/by-id/
+
+echo "--- ola ---"
+
+# Start OLA
+/usr/bin/olad -l 4 &
+
+# Wait a bit for OLA to start
+sleep 3
+
+ola_dev_info
+ls /usr/lib/ola/libola*so*
+cat /home/dmxuser/.ola/ola-usbserial.conf
+ls /usr/lib/ola/ | grep libola_
+
+# ola_patch -d 0 -u 1
+
+echo "--- bridge ---"
+
+# Start dmx2mqtt (customize with env vars if needed)
+python3 -u /app/bridge.py
