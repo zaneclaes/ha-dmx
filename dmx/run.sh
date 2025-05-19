28f7a85fd59864348890a1651b14536d1c4cb2e7
Zane Claes <zane@claes.biz>
2025-05-18 20:02:30 -0600

    v1.3.17

--- dmx/run.sh
+++ dmx/run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -ueo pipefail
 
-# chgrp dmxuser /dev/ttyUSB0
-chmod 666 /dev/ttyUSB0
+echo "--- ola ---"
 
-su - dmxuser -c 'HOME=/home/dmxuser start.sh'
+# Start OLA
+/usr/bin/olad -l 4 &
+
+# Wait a bit for OLA to start
+sleep 3
+
+ola_dev_info
+ls /usr/lib/ola/libola*so*
+cat /home/dmxuser/.ola/ola-usbserial.conf
+ls /usr/lib/ola/ | grep libola_
+
+# ola_patch -d 0 -u 1
+
+echo "--- bridge ---"
+
+# Start dmx2mqtt (customize with env vars if needed)
+python3 -u /app/bridge.py
